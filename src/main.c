cb5dbb0299ba40aac508a3ea95bb723d49f14070
EdoBergamo <hello@amtriix.it>
2023-11-05 11:36:38 +0000

    [UPDATE] Added notes base64 encode

--- src/main.c
+++ src/main.c
@@ -6,7 +6,27 @@
 // -------------------------
 // Function Signatures
 
-void base64_encode();
+void base64_encode(const unsigned char *in, const unsigned long in_len, unsigned char *out);
+
+// -------------------------
+// Core Implementation
+
+void base64_encode(const unsigned char *in, const unsigned long in_len, unsigned char *out) {
+    int in_index = 0;
+    int out_index = 0;
+
+    while (in_index < in_len) {
+        // Process group of 24 bit
+	    
+	// First 6-bit
+	
+	// Second 6-bit
+	
+	// Third 6-bit
+	
+	// Fourth 6-bit
+    }
+}
 
 int main() {
     while (!feof(stdin)) {
